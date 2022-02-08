6f0f74739ec6f41d68dcdf2cf3159cdaf2cfe09e
Rahul Chauhan <rchauhan9102@gmail.com>
2022-02-08 18:59:51 +0530

    Upload file to different path in mvc Signed-off-by: Rahul Chauhan <rchauhan9102@gmail.com>

--- MVCSearchApplication/src/main/java/com/spring/mvc/controller/FileUploadController.java
+++ MVCSearchApplication/src/main/java/com/spring/mvc/controller/FileUploadController.java
@@ -1,6 +1,13 @@
 package com.spring.mvc.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +22,25 @@ public class FileUploadController {
 	}
 
 	@RequestMapping(path = "/fileUpload", method = RequestMethod.POST)
-	public String fileupload(@RequestParam("file") CommonsMultipartFile file) {
+	public String fileupload(@RequestParam("file") CommonsMultipartFile file, HttpSession session, Model model) {
 		System.out.println(file.getOriginalFilename());
+		String path = session.getServletContext().getRealPath("/") + "WEB-INF" + File.separator + "resources"
+				+ File.separator + "images" + File.separator + file.getOriginalFilename();
+		System.out.println(path);
+		byte[] data = file.getBytes();
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(path);
+			fos.write(data);
+			fos.close();
+			model.addAttribute("msg", "File upload successful");
+			model.addAttribute("file", file.getOriginalFilename());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			model.addAttribute("msg", "File upload Failed");
+
+		}
 		return "file_upload_success";
 	}
 
--- MVCSearchApplication/src/main/webapp/WEB-INF/views/file_upload_success.jsp
+++ MVCSearchApplication/src/main/webapp/WEB-INF/views/file_upload_success.jsp
@@ -1,12 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@page isELIgnored="false"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Upload Form Result</title>
 </head>
 <body>
-
+	<h1>${msg}</h1>
+	<img alt="My File Image"
+		src="<c:url value="/resources/images/${file}"/>">
 </body>
 </html>
