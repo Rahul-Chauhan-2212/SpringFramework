d3d6594f292f8ad259d31d78d9eedab700868c3e
Rahul Chauhan <rchauhan9102@gmail.com>
2022-02-16 08:36:55 +0530

    Added Dummy Rest Service to test the Spring Security Feature
    Signed-off-by: Rahul Chauhan <rchauhan9102@gmail.com>

---
+++ spring-boot-security/src/main/java/com/spring/security/service/UserService.java
@@ -0,0 +1,33 @@
+package com.spring.security.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.security.model.User;
+
+@Service
+public class UserService {
+
+	private List<User> users = new ArrayList<>();
+
+	public UserService() {
+		users.add(new User("rahul", "rahulchauhan", "rchahan@gmail.com"));
+		users.add(new User("kiran", "kiranchauhan", "kc75633@gmail.com"));
+	}
+
+	public List<User> getAllUsers() {
+		return users;
+	}
+
+	public User getUser(String username) {
+		return users.stream().filter(u -> u.getUsername().compareTo(username) == 0).findAny().get();
+	}
+
+	public List<User> addUser(User user) {
+		users.add(user);
+		return users;
+	}
+
+}
